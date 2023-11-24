80e512394b77563d26af615483602395a07c66fb
joao Pedro Batista da Silva <joaopedro.batista.silva27@gmail.com>
2023-11-24 20:37:58 -0300

    Atualizacoes dialogo

--- Scripts/pictureManage.gd
+++ Scripts/pictureManage.gd
@@ -1,15 +1,15 @@
 extends Sprite2D
 
-var iconPadrao = preload("res://Temporario/kirbyNormal.png")
-var iconFeliz = preload("res://Temporario/kirbyFeliz.png")
-@onready var retrato = get_node("MolduraPlayer")
-var emocao: int = 1
+#var iconPadrao = preload("res://Temporario/kirbyNormal.png")
+#var iconFeliz = preload("res://Temporario/kirbyFeliz.png")
+
+var emocao: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-func changePicuture():
-	if emocao == 0:
-		retrato.set_texture(iconPadrao)
-	if emocao == 1:
-		retrato.set_texture(iconFeliz)
+#func changePicuture():
+#	if emocao == 0:
+#		print("Emoção", emocao)
+#	if emocao == 1:
+#		print("Emoção", emocao)
