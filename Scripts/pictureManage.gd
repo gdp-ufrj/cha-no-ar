6d5bf41f4eccfc4294af5bd644a5e3912ebc283c
Rafahgol <rafalima245@hotmail.com>
2023-12-01 18:34:10 -0300

    protótipo de emoções funcionando

--- Scripts/pictureManage.gd
+++ Scripts/pictureManage.gd
@@ -1,16 +1,21 @@
 extends Sprite2D
 
-var emote = preload("res://Scripts/state.gd").new()
-#var iconFeliz = preload("res://Temporario/kirbyFeliz.png")
-
+var iconPadrão = preload("res://Temporario/kirbyNormal.png")
+var iconFeliz = preload("res://Temporario/kirbyFelizResized.png")
+var iconTriste = preload("res://Temporario/kirbyTristeResized.png")
+@onready var moldura_player = $"."
 #var emocao: int = 0
 # Called when the node enters the scene tree for the first time.
-var emocao_count = emote.getEmocao()
-func _ready():
-	print(emote.getEmocao())
-	pass
+
+func _process(delta):
+	changePicuture()
+	print(State.emocao_count)
 func changePicuture():
-	if emocao_count == 0:
-		print("cai no 1 if")
-	if  emocao_count == 1:
-		print("cai no 2 if")
+	if State.emocao_count == 0:
+		moldura_player.texture = iconPadrão
+	if State.emocao_count == 1:
+		moldura_player.texture = iconFeliz
+#		moldura_player.scale.x = 0.213
+#		moldura_player.scale.y= 0.213
+	if  State.emocao_count == 2:
+		moldura_player.texture = iconTriste
