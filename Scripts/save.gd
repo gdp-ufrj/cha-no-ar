64e9b328d992cde84180d3be56f4743848e9d283
IgorDelfino <60139386+IgorDelfino@users.noreply.github.com>
2023-12-04 12:34:23 -0300

    added basic dialogue score system

---
+++ Scripts/save.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+func save():
+	var route_data = {
+		
+	}
--- Scripts/state.gd
+++ Scripts/state.gd
@@ -4,3 +4,5 @@ var current_scene = "fase"
 var transition_scene = false
 
 var capuccino_count: int = 1
+
+var adonis_love = 0
