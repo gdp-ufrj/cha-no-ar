498d5f901f7b242087e7f15c563d664ad9ff5114
IgorDelfino <igordelfinobarbosa56@gmail.com>
2023-12-14 13:48:28 -0300

    drinks menu scene

--- Scripts/livro_de_receitas.gd
+++ Scripts/livro_de_receitas.gd
@@ -7,10 +7,19 @@ var card = preload("res://Scenes/Cards.tscn")
 
 var drinks: Array
 var page_limit: int
-var current_page: int
+var current_page: int = 0
+@onready var drink_sprite: Sprite2D = get_node("Background/Drink Sprite")
+@onready var drink_name_label: Label = get_node("Background/Drink Name")
+@onready var drink_ingredients_label: Label = get_node("Background/Ingredients Label/Drink Ingredients")
+@onready var drink_description_label: RichTextLabel = get_node("Background/Description Label/Drink Description")
+@onready var next_drink_button: Button = get_node("Background/Next Tea")
+@onready var previous_drink_button: Button = get_node("Background/Previous Tea")
+@onready var make_tea_button: Button = get_node("Background/Make Tea")
 
 func _ready():
 	get_drink_resources()
+	set_info(drinks[0])
+	check_buttons()
 		
 func get_drink_resources():
 	var path = "res://Resources/Drinks/"
@@ -26,7 +35,35 @@ func get_drink_resources():
 	print(page_limit)
 	
 func set_info(drink: Drink_Resource):
+	if drink.Drink_Image:
+		drink_sprite.texture = load(drink.Drink_Image)
+	else: 
+		drink_sprite.texture = load("res://Assets/sprites/ui/Chá.png")
+	drink_name_label.text = drink.Drink_Name
+	drink_ingredients_label.text = drink.Drink_Description
+	drink_description_label.text = drink.Drink_Description
 	
+func next_drink():
+	current_page = current_page + 1
+	set_info(drinks[current_page])
+	check_buttons()
+	
+func previous_drink():
+	current_page = current_page - 1
+	set_info(drinks[current_page])
+	check_buttons()
+	
+func check_buttons():
+	if current_page == 0:
+		previous_drink_button.disabled = true
+	else:
+		previous_drink_button.disabled = false
+		
+	if current_page == page_limit:
+		next_drink_button.disabled = true
+	else:
+		next_drink_button.disabled = false
+		
 
 func toggle_recipe_book():
 	aberto = !aberto
